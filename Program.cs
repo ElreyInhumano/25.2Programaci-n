1d36f5bc47e7b1e9d03bef5ccdc370a35b68ee38
ElreyInhumano <mqgareth@gmail.com>
2025-09-08 17:09:39 -0500

    commit inicial
    Matías Quintanilla

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25._2Ejercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
